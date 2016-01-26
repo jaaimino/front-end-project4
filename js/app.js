bf9ab6a640afac6d7c575f2a71a8dce61cfea22f
jaaimino <jacobaimino@gmail.com>
2016-01-26 18:40:12 +0000

    Updated dependencies. Lots more modularity

--- js/app.js
+++ js/app.js
@@ -1,41 +1,13 @@
-var myFirebaseRef = new Firebase("https://front-end-project4.firebaseio.com/");
+/*
+ * Main app stuff here 
+ * All DOM manipulation in here
+ */
 
-myFirebaseRef.child("location/city").on("value", function(snapshot) {
-  console.log(snapshot.val());  // Alerts "San Francisco"
-});
-
-myFirebaseRef.set({
-  title: "Hello World!",
-  author: "Firebase",
-  location: {
-    city: "San Francisco",
-    state: "California",
-    zip: 94103
-  }
-});
-
-myFirebaseRef.child("location/city").on("value", function(snapshot) {
-  console.log(snapshot.val());  // Alerts "San Francisco"
-});
-
-myFirebaseRef.createUser({
-  email    : "bobtony@firebase.com",
-  password : "correcthorsebatterystaple"
-}, function(error, userData) {
-  if (error) {
-    console.log("Error creating user:", error);
-  } else {
-    console.log("Successfully created user account with uid:", userData.uid);
-  }
-});
-
-myFirebaseRef.authWithPassword({
-  email    : "bobtony@firebase.com",
-  password : "correcthorsebatterystaple"
-}, function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-  }
+initFirebase()
+logout();
+setStuff();
+login(function(){
+  console.log("Logged in before calling stuff.");
+  console.log(getCurrentUser());
+  setStuff();
 });
---
+++ js/firebaseutil.js
@@ -0,0 +1,74 @@
+/* global Firebase */
+
+//Other local vars to this file
+var firebaseURL = "https://front-end-project4.firebaseio.com/";
+var firebaseRef = null;
+var currentUser = null;
+
+function initFirebase(){
+    firebaseRef = new Firebase(firebaseURL);
+}
+
+function createUser(callback){
+    getFirebase().createUser({
+      email    : "bobtony@firebase.com",
+      password : "correcthorsebatterystaple"
+    }, function(error, userData) {
+      if (error) {
+        console.log("Error creating user:", error);
+      } else {
+        console.log("Successfully created user account with uid:", userData.uid);
+        callback(userData);
+      }
+    });
+}
+
+function login(callback){
+    getFirebase().authWithPassword({
+      email    : "bobtony@firebase.com",
+      password : "correcthorsebatterystaple"
+    }, function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+        setCurrentUser(authData);
+        callback(authData);
+      }
+    });
+}
+
+function logout(){
+    getFirebase().unauth();
+}
+
+function setStuff(){
+    console.log("Setting stuff");
+    getFirebase().child("posts").push({
+      title: "Hello World!",
+      author: "Firebase",
+      text: "Stuff!",
+      location: {
+        city: "San Francisco",
+        state: "California",
+        zip: 94103
+      }
+    });
+}
+
+/**
+ * Getters and setters
+ */
+
+function getFirebase(){
+    return this.firebaseRef;
+}
+
+function getCurrentUser(){
+    return currentUser;
+}
+
+function setCurrentUser(userData){
+    currentUser = userData;
+}
+
