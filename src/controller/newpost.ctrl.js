9e4f8f3d79b0dea3becb671e342b85923584a0e6
jaaimino <jacobaimino@gmail.com>
2016-01-29 01:53:27 +0000

    Attempting to select stuff with jquery inside controllers. Might be a scope issue. Needs to be fixed

--- src/controller/newpost.ctrl.js
+++ src/controller/newpost.ctrl.js
@@ -1,6 +1,11 @@
 /* global getControllers */
 addController("newpost", {
     setup : function(){
+      console.log($("#nope").text());
+        $("#postForm").submit(function( event ) {
+          alert( "Handler for .submit() called." );
+          event.preventDefault();
+        });
     },
     renderPage : function(){
     }
