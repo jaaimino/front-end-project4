c2e24dedc8ea8f9a214029e46b9dbca19ce89037
jaaimino <jacobaimino@gmail.com>
2016-02-01 22:31:07 +0000

    Added possibility for multiple css themes

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,18 @@ var stylus = require('gulp-stylus');
 var nib = require('nib');
 
 // Get one .styl file and render
-gulp.task('one', function () {
-  gulp.src('./stylus/style.styl')
+gulp.task('light', function () {
+  gulp.src('./stylus/lightstyle.styl')
     .pipe(stylus({ use: nib(), compress: true}))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('./css'));
+});
+
+// Get one .styl file and render
+gulp.task('dark', function () {
+  gulp.src('./stylus/darkstyle.styl')
+    .pipe(stylus({ use: nib(), compress: true}))
+    .pipe(gulp.dest('./css'));
 });
 
 // Default gulp task to run
-gulp.task('default', ['one']);
+gulp.task('default', ['light', 'dark']);
